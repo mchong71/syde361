8c5e3eb4128ed33cc03bbcc051f8f1e4b42fcc8f
Veronica Vermeulen <veronica.m.vermeulen@gmail.com>
2012-07-15 16:48:14 -0400

    Landing Page

---
+++ LandingPage.php
@@ -0,0 +1,13 @@
+ <p><b><big>Welcome to BufferBox</b></big></p>
+
+<p>Please select one of the following options:</p>
+
+<FORM>
+<INPUT TYPE="BUTTON" VALUE="Stock Bufferbox" ONCLICK="window.location.href='http://www.computerhope.com'"> 
+</FORM>
+<FORM>
+<INPUT TYPE="BUTTON" VALUE="Retrieve a Package" ONCLICK="window.location.href='http://www.computerhope.com'"> 
+</FORM>
+<FORM>
+<INPUT TYPE="BUTTON" VALUE="Add Lockers" ONCLICK="window.location.href='http://www.computerhope.com'"> 
+</FORM>
