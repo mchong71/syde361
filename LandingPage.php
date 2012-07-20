a2ab55c5697ccacb52ce169d1d7bcae406613863
Veronica Vermeulen <veronica.m.vermeulen@gmail.com>
2012-07-19 23:53:29 -0400

    Sal made it prettier
    But he's still a jerk

--- LandingPage.php
+++ LandingPage.php
@@ -1,6 +1,11 @@
+<body style="background-color:#000; margin:0 auto; text-align=center;padding:50px;color:#fff;font:12px Arial;" >
+
+<div style="border:1px solid #fff; padding:10px;">
+
  <p><b><big>Welcome to BufferBox</b></big></p>
 
 <p>Please select one of the following options:</p>
+
 
 <FORM>
 <INPUT TYPE="BUTTON" VALUE="Stock Bufferbox" ONCLICK="window.location.href='http://www.computerhope.com'"> 
@@ -11,3 +16,5 @@
 <FORM>
 <INPUT TYPE="BUTTON" VALUE="Add Lockers" ONCLICK="window.location.href='http://www.computerhope.com'"> 
 </FORM>
+
+</div>
