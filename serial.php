bbaafc3e1e507eedb100ed6211d1a1f27f607509
Scott Neil <dscott.neil@gmail.com>
2012-07-20 15:28:22 -0400

    added singleton contructor to serial

--- serial.php
+++ serial.php
@@ -1,27 +1,38 @@
-<?php>
+<?php
 include 'php_serial.php';
 
 class serial 
 {
-	// if os is linux /dev/ttys0 FIX THIS LATER
-	var $com_string = '/dev/cu.usbmodem621';
-	$serial = new phpserial();
-	$serial->deviceSet($com_string);
-		
-	$serial->confBaudRate(9600); //Baud rate: 9600 
-    $serial->confParity("none");  //Parity (this is the "N" in "8-N-1")
-    $serial->confCharacterLength(8); //Character length 
-    $serial->confStopBits(1);  //Stop bits (this is the "1" in "8-N-1") 
-    //$serial->confFlowControl("none");
-	//Device does not support flow control of any kind, 
-	//so set it to none. 
-	
-    //Now we "open" the serial port so we can write to it 
-    $serial->deviceOpen(); 
-
+	$portCreated = false;
 	/* function will query the serial port until it returns a value.
 		Nothing else can happen while querying the port.
 	*/
+	
+	function serial() 
+	{
+		//the first time this class is instantiated the port will be setup. Otherwise do nothing
+		if(!$portCreated) {
+			// if os is linux /dev/ttys0 FIX THIS LATER
+			var $com_string = '/dev/cu.usbmodem621';
+			$serial = new phpserial();
+			$serial->deviceSet($com_string);
+			
+			$serial->confBaudRate(9600); //Baud rate: 9600 
+   		 	$serial->confParity("none");  //Parity (this is the "N" in "8-N-1")
+		    $serial->confCharacterLength(8); //Character length 
+		    $serial->confStopBits(1);  //Stop bits (this is the "1" in "8-N-1") 
+		    //$serial->confFlowControl("none");
+			//Device does not support flow control of any kind, 
+			//so set it to none. 
+	
+	  		//Now we "open" the serial port so we can write to it 
+		    $serial->deviceOpen();
+		    
+		    //successful open of port
+		    $portCreated = true;
+	    }
+	}
+	
 	function readMsg()
 	{
 		do 
