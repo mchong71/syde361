6091a5e24111378c9a3572a08ef933793361aff3
Scott Neil <dscott.neil@gmail.com>
2012-07-20 21:43:02 -0400

    add serial write funcitonality and logic to getBoxes

--- getBoxes.php
+++ getBoxes.php
@@ -2,50 +2,34 @@
 <head>
 
 <?php
-include 'php_serial.php';
+include 'messaging.php';
 
-function readMsg($fd) {
-
-	/*$response = "";
-	$data = serial->readPort();
-	echo "curD: " . $data;
-	while($data != chr(10))
-	{
-		$response = $response . $data;
-		$data = serial->readPort();
-	}*/
-
-	return "C24";//$response;
-}
+$_BOX = -1;
+$_COL = -1;
+$_COMPARTMENT = -1;
+$_PACKINTSIZE = -1;
 
 function get_Size($size) {
+	global $_BOX, $_COL, $_COMPARTMENT, $_PACKINTSIZE;
 	$dbhost = "localhost";
 	$dbname = "kiosk_map";
 	$dbuser = "root";
 	$dbpass = "root";
 	$link = mysql_connect("$dbhost", "$dbuser", "$dbpass") or die (mysql_error());
 	mysql_select_db("$dbname") or die(mysql_error());
-	$com_string = '/dev/cu.usbmodem621';//'COM1';
-	
-	// Formulate Query
-	//$query = sprintf("SELECT Package_ID, Column_ID, Box_ID, Filled, Open, Size FROM States");
-
-	// Perform Query
-	//$result = mysql_query($query);
 	
 	//Redo This!
-	$packIntSize = -1;
 	switch($size) {
 		case 'S':
-			$packIntSize = 0;
+			$_PACKINTSIZE = 0;
 			break;
 		
 		case 'M':
-			$packIntSize = 1;
+			$_PACKINTSIZE = 1;
 			break;
 		
 		case 'L':
-			$packIntSize = 2;
+			$_PACKINTSIZE = 2;
 			break;
 		
 		default:
@@ -53,7 +37,7 @@ function get_Size($size) {
 			break;
 	}
 	
-	$query = sprintf("SELECT * From States WHERE Size='" .$packIntSize. "' && Filled='0' LIMIT 1");
+	$query = sprintf("SELECT * From States WHERE Size='" .$_PACKINTSIZE. "' && Filled='0' LIMIT 1");
 	$result = mysql_query($query);
 	
 	if (!$result) {
@@ -63,92 +47,84 @@ function get_Size($size) {
 	} 
 	
 	$resultArr = mysql_fetch_array($result);
-	$selectedCol = $resultArr['Column_ID'];
-	$selectedBox = $resultArr['Box_ID'];
-	$selectedComp = $selectedCol . $selectedBox;
+	$_BOX = $resultArr['Column_ID'];
+	$_COL = $resultArr['Box_ID'];
+	$_COMPARTMENT = $_BOX . $_COL;
 
-	echo ($selectedCol . " | " . $selectedBox);
+	echo ($_COMPARTMENT);
+	
+	//free the result
+	mysql_free_result($result);
 
 	// set up serial connection
-	$serial = new phpSerial();
-	$serial->deviceSet($com_string);
-	$serial->confBaudRate(9600); //Baud rate: 9600 
-    $serial->confParity("none");  //Parity (this is the "N" in "8-N-1")
-    $serial->confCharacterLength(8); //Character length 
-    $serial->confStopBits(1);  //Stop bits (this is the "1" in "8-N-1") 
-    
-    //$serial->confFlowControl("none");
-	//Device does not support flow control of any kind, 
-	//so set it to none. 
-	
-    //Now we "open" the serial port so we can write to it 
-    $serial->deviceOpen(); 
-	$serial->sendMessage("U" . $selectedComp .chr(10));
+	$serialp = new messaging;
+	$serialp->writeMsg("U", $_COMPARTMENT);
 	
 	echo 'Message has been Sent!';
+	//$serialp->writeMsg("L", $_COMPARTMENT);
+	//$limStat = $serialp->readMsg();
+	//packageProcessing();	
+}
+
+function doorClosed() {
+	global $_BOX, $_COL, $_COMPARTMENT, $_PACKINTSIZE;
+	echo "indoors";
+	$serialp = new messaging;
+	$serialp->writeMsg("S", $_COMPARTMENT);
+	$sensorData = 1;
 	
-	$door_opened = 0;
-
-	//They loop this 20 times to allow for any errors. Look into a better way to do this
-	$i=0;
-	while($i < 20)
-	{
-	//Wait for response from arduino's. Query arduino's. Wait for Matt
-    $data = readMsg();
-
-    if($data)
-    {
-        $data = trim($data);
-
-        if($data == "O" . $selectedComp)
-        {
-        	echo "YSY";
-            $door_opened = 1;
-            
-            //set dummy package ID. We'll fix this later
-            /*$pack_ID = (i*3)+($selectedBox*2);
-            
-            //putting the new parcel into the bb. Sets the package ID and filled status
-			$query = sprintf("UPDATE States Set Filled = 1, Package_ID='" .$pack_ID. "' Where Size='" .$packIntSize. "' && Filled='0' && Column_ID='" .$selectedCol."' Box_ID='" .$selectedBox."'");
-			$result = mysql_query($query);
+	if($sensorData == 0) {
+		//unfilled - FAIL
+		//die();
+		
+	} elseif($sensorData == 1) {
+		//filled - SUCCESS!
+		$pack_ID = (i*3)+($_BOX*2);
+           
+        //putting the new parcel into the bb. Sets the package ID and filled status
+		$query = sprintf("UPDATE States Set Filled = 1, Package_ID=" .$pack_ID. " Where Size=" .$_PACKINTSIZE. " && Filled=0 && Column_ID=" .$_COL." && Box_ID=" .$_BOX);
+		$result = mysql_query($query);
 	
-			if (!$result) {
-    			$message  = 'Invalid query: ' . mysql_error() . "\n";
-	    		$message .= 'Whole query: ' . $query;
-    			die($message);
-			}
-
-			//test to ensure the is an active internet connection
-            /*$conn = @fsockopen("www.google.com", 80, $errno, $errstr, 30);
-
-            if($conn)
-            {
-            	//This seems to be writing to an offsite BB db
-            }*/
-
-            //echo '<META HTTP-EQUIV="Refresh" Content="0; URL=http://localhost/DoorOpened.html?compartment=' . $compartment . '">';
-            break;
-        }
-        elseif($data == "C" . $selectedComp)
-        {
-        	//If door is not open, attempt to send message again
-        	$serial->sendMessage("U" . $selectedComp .chr(10));
-        }
-        else { echo "failed to open";}
-    }
-    
-    //query status?
-
-    usleep(100000);
-    $i++;
+		if (!$result) {
+    		$message  = 'Invalid query: ' . mysql_error() . "\n";
+	   		$message .= 'Whole query: ' . $query;
+    		die($message);
+		}
+		
+		echo "Input Package!!!!";
 	}
+}
 
-//The reaction if the door failed to open
-//include method here
-
-	$serial->deviceClose(); 
-	mysql_free_result($result);
-}		
+function packageProcessing() {
+	global $_BOX, $_COL, $_COMPARTMENT;
+	$serialp = new messaging;
+	$serialp->writeMsg("L", $_COMPARTMENT);
+	$lockData = 1;//$serialp->readMsg();//get message data from serial
+	
+	if($lockData == 0) {
+		$serialp->writeMsg("U", $_COMPARTMENT);
+		packageProcessing();
+		
+	} elseif($lockData == 1) { //success in opening
+		//spam backend until door is closed
+		$limitData = 1;
+		
+		while($limitData != 0) {
+			$serialp->writeMsg("L", $_COMPARTMENT);
+			echo "checked for limit";
+			$limitData = 0;
+		}
+		
+		//ensure door is closed
+		if($limitData == 0) {
+			echo "door is closed";
+			doorClosed();
+		} else {
+			die();
+		}
+	}
+}
+		
 ?>
 </head>
 <body>
