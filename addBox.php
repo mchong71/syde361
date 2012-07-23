01dee9730af314c1d439de4072e5149258010aa0
Matthew Chong <matt.chong45@gmail.com>
2012-07-23 13:38:41 -0400

    fixed addBox.php and addbox_form to handle serial reading and writing

--- addBox.php
+++ addBox.php
@@ -5,10 +5,8 @@
 
 include 'messaging.php';
 
-$serial = new messaging();
-//sends message to backend telling them to expect a new locker
-$serial->writeMsg("N");
-$sizArr = $serial->readMsg();
+
+//$sizeArr = array(0 => 1, 1 => 2, 2 => 3, 3 => 1, 4 => 2, 5 => 3);
 
 //Get COM Port
 	$dbhost = "localhost";
@@ -17,29 +15,32 @@ $sizArr = $serial->readMsg();
 	$dbpass = "root";
 	$link = mysql_connect("localhost", "root", "root") or die (mysql_error());
 	mysql_select_db("kiosk_map") or die(mysql_error());
-
 	
-function addBox($sizeArr) {
-
+function addBox() {
+	$serial = new messaging;
+	//sends message to backend telling them to expect a new locker
+	$serial->writeMsg("N");
+	$sizeArr = $serial->readMsg();
 	$numOfBox = Count($sizeArr);
 	//get the next available column_ID
 	// HAVE TO REFRESH PAGE TO GET THE NEW MAX
 	$column_ID = get_SQLarray("Select MAX(Column_ID) as Column_ID from States");
 	$max_ID = $column_ID['Column_ID'];
-	echo $max_ID;
 	//inserts all the new boxes into the database. Box_ID is 1 based
-	for ($i = 0; $i < $numOfBox; $i++)
+	for ($i = 1; $i < $numOfBox; $i++)
 	{
 		$boxNum = $i + 1;
 		$new_ID = $max_ID + 1;
 		mysql_query("Insert into States values (0," . $new_ID . "," . $boxNum . ",0,0," . $sizeArr[$i] . ")"); 
 	}
+	echo "You have successfully added " . $numOfBox . " boxes!";
 
 }
 
 function get_SQLarray($query){
 	$result = mysql_query($query);
 	$array = mysql_fetch_array($result);
+	mysql_free_result($array);
 	return $array;
 }
 ?>
@@ -49,8 +50,7 @@ function get_SQLarray($query){
 
 <!-- addBox could only take in an array where the first element is the number of
 		boxes the next elements are the corresponding sizes -->
-<Input type = 'button' Name = 'button1' onclick = "<? addBox($sizeArr) ?>" Value = "Add box"/>
-You have successfully added <? $numOfBox ?> boxes.
+<?php addBox();?>
 
 </body>
 </html>
---
+++ addbox_form.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<body>
+
+<form action="addbox.php" method="post">
+<input type="submit" />
+
+</form>
+</body>
+</head>
+</html>
