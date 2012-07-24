8ccaa4c5ddfaf47bc97bc2b100e8a3a7662fe031
Surbhi Jhavar <surbhijhavar@v1416-wn-172-21-65-249.campus-dynamic.uwaterloo.ca>
2012-07-24 01:17:22 -0400

    Linked all the pages

--- PackageRetrieval.php
+++ PackageRetrieval.php
@@ -1,22 +1,56 @@
 <html>
-<head>
-</head>
-<body style="background-color:#000; margin:0 auto; text-align=center;padding:50px;color:#fff;font:12px Arial;">
 
+<style type="text/css">
 
-<div style="border:1px solid #fff; padding:10px;">
-	<p>Input package attributes and submit form.</p></br>
-	
-	<form action="getBoxes.php" method="post">
-		<b>Box Size (S/M/L):</b>
-		</br><input type="text" name="pSize" style="width:150px; height:20px;" />
-		</br><b>Enter Specific Box:</b>
-		</br> Column: <input type="text" name="column" style="width:150px; height:20px;"/>
-		</br> Box: <input type="text" name="box" style="width:150px; height:20px;"/>
-		</br>
-		<input type="submit" style="width:80px; height:130px;"/>
-	</form>
-	
-</div>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+img#bg {
+  position:fixed;
+  top:0;
+  left:0;
+  width:100%;
+  height:100%;
+}
+
+#content {
+  position:relative;
+  z-index:1;
+}
+
+</style>
+
+<body style="margin:0 auto; text-align=center;padding:50px;color:#fff;font:12px Arial;">
+<img src="GreenBackground.jpg" alt="background image" id="bg" />
+
+<body>
+<div id="content">
+
+<p><b><FONT size ="16"><big>Package Retrieval</b></FONT></p></br>
+
+  <form action="getBoxes.php" method="post">
+    <b>Box Size (S/M/L):</b>
+    </br><input type="text" name="pSize" style="width:150px; height:20px;" /><br><br>
+    </br><b>Enter Specific Box:</b>
+    </br> Column: <input type="text" name="column" style="width:150px; height:20px;"/><br>
+    </br> Box: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="box" style="width:150px; height:20px;"/>
+    </br><br>
+<input type="submit" style="width:80px; height:130px;"/>
+  </form>
+
+  <!-- <form action="pickup.php" method="post">
+ <p><b><FONT size ="12"><CENTER> <big>PICKUP</b></big></CENTER></FONT></p>
+Please Enter your Package ID from BB: <input type="text" name="packageID" />
+<input type="submit" />
+
+</form> -->
+<!-- <form action="getBoxes.php" method="post">
+<FONT SIZE=+1 COLOR="white">Box Size (S/M/L): </FONT><input type="text" name="pSize"  style="width:150px; height:20px;"/><br><br><br>
+<input type="submit" style="width:80px; height:130px;" />
+</form> -->
+
 </body>
 </html>
